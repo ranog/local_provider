fbce076071bcbe6fc41696e71f5461224b6e3403
ranog <jprnogueira@yahoo.com.br>
2022-02-07 13:55:10 -0300

    creating the local file resource

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "local_file" "literature" {
   filename = "art_of_war.txt"
   content   =  <<-EOT
         Sun Tzu said: The art of war is of vital importance to the State.
+
         It is a matter of life and death, a road either to safety or to
         ruin. Hence it is a subject of inquiry which can on no account be
         neglected.
